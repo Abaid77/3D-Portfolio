c6b58ccf694ce7bf515248678e22faaf4c5e0e20
Amit Baid <baidstar@hotmail.com>
2023-10-16 18:23:57 -0600

    add 3d earth and stars and connect emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,9 +17,45 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_csqak39',
+        'template_5b5ug5d',
+        {
+          from_name: form.name,
+          to_name: 'Amit',
+          from_email: 'form.email',
+          to_email: 'amit.baid@hotmail.com',
+          message: form.message,
+        },
+        'MtVbj8KRaykQG-Mu1'
+      )
+      .then(() => {
+        setLoading(false);
+        alert('Thank you I will get back to you as soon as possible');
+        setForm(
+          {
+            name: '',
+            email: '',
+            message: '',
+          },
+          (error) => {
+            setLoading(false);
+            console.log(error);
+            alert('Oops something went wrong. Please try again.');
+          }
+        );
+      });
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
